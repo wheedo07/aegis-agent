9a842740f25f19e850df96853024078270c96a9e
brillianintdoh <wheedo07@ourgram.co.kr>
2026-03-09 15:49:40 +0900

    .gitignore에 build 디렉토리 추가 및 build.sh 파일 생성

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+dir=$(basename "$PWD")
+if [[ "$dir" != "agent" && "$dir" !=  "aegis-agent" ]]; then
+    echo "프로젝트 ROOT에서 실행해주세요"
+    exit 1
+fi
+
+if [ ! -d "build" ]; then
+    mkdir build
+fi
+
+cd module && zip ../build/aegis-mod.zip *
