933ac971ec3c0fa1eb2a1da2276997454252b894
brillianintdoh <wheedo07@ourgram.co.kr>
2026-03-12 01:11:09 +0900

    에이전트 HTTP 서버 클래스 구현 및 요청 처리 메서드 추가

--- src/agent.http.cpp
+++ src/agent.http.cpp
@@ -1,1 +1,12 @@
 #include "agent.http.h"
+
+AegisHttpServer::AegisHttpServer() {
+    curl = curl_easy_init();
+}
+
+AegisHttpServer::~AegisHttpServer() {
+    if(curl) curl_easy_cleanup(curl);
+}
+
+AegisHttpResponse AegisHttpServer::handle_request(AegisMessage msg) {
+}
--- src/agent.http.h
+++ src/agent.http.h
@@ -1,9 +1,25 @@
 #ifndef __AEGIS_AGENT_HTTP_H__
 #define __AEGIS_AGENT_HTTP_H__
+#include "agent.socket.h"
 #include<curl/curl.h>
 #include<json/json.h>
+#include<string>
+using namespace std;
+
+typedef struct {
+    bool block = false;
+    string reason;
+} AegisHttpResponse;
 
 class AegisHttpServer {
+    private:
+        CURL *curl;
+        
+    public:
+        AegisHttpServer();
+        ~AegisHttpServer();
+
+        AegisHttpResponse handle_request(AegisMessage msg);
 };
 
 #endif
